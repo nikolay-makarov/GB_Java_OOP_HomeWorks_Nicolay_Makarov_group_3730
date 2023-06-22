3ec36495aadfdbfa8a3adb8662fa567bcd7de3f3
Nicolay Makarov <i.n.makarov@mail.ru>
2023-06-22 13:08:59 +0500

    Шестое домашнее задание

---
+++ HomeWork06/srp2/Program.java
@@ -0,0 +1,12 @@
+package OOP.HomeWorks.HomeWork06.srp2;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Order order = new Order();
+        Input input = new Input();
+        SaveTo saveTo = new SaveTo();
+        input.inputFromConsole(order);
+        saveTo.saveToJson(order);
+    }
+}
